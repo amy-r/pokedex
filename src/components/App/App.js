b2d23eb6d84e8bdf4c72c425bde62e888ab68aa5
Amy Russell <russell.amyh@gmail.com>
2018-02-22 11:01:56 -0700

    something weird going on with my redux setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import FakeContainer from '../../containers/FakeContainer/'
 import { getPokemonTypes } from '../../helper.js';
+import { connect } from 'react-redux';
+import { addType } from '../../actions';
+
 
 class App extends Component {
 
-
+constructor() {
+  super()
+}
 // we want to call the api to get types on on component did mount
 // then we want to async/await/ resolve the promises that we get/json/do the things to get our data in usable form
 // send types of pokemon in the store
 // pass down store through props
 // display pokemon types in connected components
 
-  componentDidMount = () => {
-    getPokemonTypes();
+  componentDidMount = async () => {
+    const pokeTypes = await getPokemonTypes();
+    console.log(this.props)
+    // this.props.addTypes(pokeTypes);
   }
 
   render() {
@@ -27,4 +34,10 @@ class App extends Component {
 }
 
 
-export default App;
+// const mapStateToProps = ({ type }) => ({ type });
+
+const mapDispatchToProps = dispatch => (
+  { addTypes: (pokeTypes) => dispatch(addTypes(pokeTypes)) }
+);
+
+export default connect(null, mapDispatchToProps)(App);
